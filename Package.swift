62579a85f183ab594ca5f3c555aabbaf1f49a000
Gian Calgeer <gian.calgeer@phenixrts.com>
2025-04-11 12:39:56 +0200

    Release Apple SDK 2025.0.1 (#STRM-2762) In Progress
    * Update to version 2025.0.1-beta.50
    
    Change-Id: Ia4b6753ddfe3d2bb5f7853417fbea9e7a978fe61
    Reviewed-on: https://gerrit.phenixrts.com/c/swift-phenixrtssdk-drm-castlabs/+/19406
    Tested-by: Andreas Schuler <andreas.schuler@phenixrts.com>
    Reviewed-by: Philip Joseph <philip.joseph@phenixrts.com>
    Reviewed-by: Tim Gronkowski <tim.gronkowski@phenixrts.com>
    Reviewed-by: Andreas Schuler <andreas.schuler@phenixrts.com>

--- Package.swift
+++ Package.swift
@@ -7,19 +7,19 @@ let package = Package(
     platforms: [.iOS(.v12), .tvOS(.v12),],
     products: [
         .library(name: "PhenixSdkDrmCastLabs",
-                 targets: ["PhenixSdkDrmCastLabs", "PhenixSdkDrmCastLabsWithDeps"]),
+                 targets: ["PhenixSdkDrmCastLabsBinary", "PhenixSdkDrmCastLabs"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/phenixrts/swift-phenixrtssdk", from: "2025.1.0"),
-        .package(url: "https://github.com/PhenixRTSVendors/swift-castlabs-isobmff-player", from: "1.0.0"),
+        .package(url: "https://github.com/phenixrts/swift-phenixrtssdk", from: "2025.0.1-dee89c9-20250411T085613335-beta.50"),
+        .package(url: "https://github.com/PhenixRTSVendors/swift-castlabs-isobmff-player", from: "1.0.4"),
     ],
     targets: [
         .binaryTarget(
                 name: "PhenixSdkDrmCastLabsBinary",
-                url: "https://dl.phenixrts.com/sdk/apple/2025.0.0-5db9bb8-20250117T110544222/PhenixSdkDrmCastLabs.xcframework.zip",
-                checksum: "240b6ee5d672fdcc4c5976cdb840d3bdb78a11a8ce3038d71a5b0adee8a4f50f"),
+                url: "https://dl.phenixrts.com/sdk/apple/2025.0.1-dee89c9-20250411T085613335/PhenixSdkDrmCastLabs.xcframework.zip",
+                checksum: "19e9ab338d5c2f6cf1a92720e733d29f348ed5e47059d1ba836804c095745d54"),
         .target(
-                name: "PhenixSdkDrmCastLabsWithDeps",
-                dependencies: [.product(name: "PhenixSdk", package: "swift-phenixrtssdk"), .product(name: "CastlabsIsobmffPlayer", package: "swift-castlabs-isobmff-player")]),
+                name: "PhenixSdkDrmCastLabs",
+                dependencies: [.product(name: "PhenixSdk", package: "swift-phenixrtssdk"), .product(name: "CastLabsIsoBmffPlayer", package: "swift-castlabs-isobmff-player")]),
     ]
 )
