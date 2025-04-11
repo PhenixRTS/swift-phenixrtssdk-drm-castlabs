f016e896427b8cbe0ee2a8a624858d40fd2df89f
Gian Calgeer <gian.calgeer@phenixrts.com>
2025-04-11 18:48:14 +0200

    Release Apple SDK 2025.0.1 (#STRM-27652) In Progress
    * Rename the targets so that the target containing the PhenixSdkDrmCastLabs module is named PhenixSdkDrmCastLabs
    * This fixes a build error in the Protected Channel Viewer
    
    Change-Id: I9d1ae16ba05dfd2482f021c716052df93dddeeda
    Reviewed-on: https://gerrit.phenixrts.com/c/swift-phenixrtssdk-drm-castlabs/+/19412
    Reviewed-by: Andreas Schuler <andreas.schuler@phenixrts.com>
    Tested-by: Gian Calgeer <gian.calgeer@phenixrts.com>
    Reviewed-by: Gian Calgeer <gian.calgeer@phenixrts.com>

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     platforms: [.iOS(.v12), .tvOS(.v12),],
     products: [
         .library(name: "PhenixSdkDrmCastLabs",
-                 targets: ["PhenixSdkDrmCastLabsBinary", "PhenixSdkDrmCastLabs"]),
+                 targets: ["PhenixSdkDrmCastLabsWithDependencies"]),
     ],
     dependencies: [
         .package(url: "https://github.com/phenixrts/swift-phenixrtssdk", from: "2025.0.1-dee89c9-20250411T085613335-beta.50"),
@@ -15,11 +15,11 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-                name: "PhenixSdkDrmCastLabsBinary",
+                name: "PhenixSdkDrmCastLabs",
                 url: "https://dl.phenixrts.com/sdk/apple/2025.0.1-dee89c9-20250411T085613335/PhenixSdkDrmCastLabs.xcframework.zip",
                 checksum: "19e9ab338d5c2f6cf1a92720e733d29f348ed5e47059d1ba836804c095745d54"),
         .target(
-                name: "PhenixSdkDrmCastLabs",
-                dependencies: [.product(name: "PhenixSdk", package: "swift-phenixrtssdk"), .product(name: "CastLabsIsoBmffPlayer", package: "swift-castlabs-isobmff-player")]),
+                name: "PhenixSdkDrmCastLabsWithDependencies",
+                dependencies: ["PhenixSdkDrmCastLabs", .product(name: "PhenixSdk", package: "swift-phenixrtssdk"), .product(name: "CastLabsIsoBmffPlayer", package: "swift-castlabs-isobmff-player")]),
     ]
 )
